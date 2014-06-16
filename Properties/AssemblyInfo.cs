e71b52d194ddebfe80c2de01a87c1044341fb161
Stepan Sychev <stepan.sychev@gmail.com>
2014-06-16 12:07:43 +0700

    Add author for assembly

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("no company. For own use only.")]
 [assembly: AssemblyProduct("Helpers")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Sychev Stepan ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
