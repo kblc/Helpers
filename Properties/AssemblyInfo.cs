6bd54f09d13553ba499d01668a34041f6834d220
stepan.sychev@gmail.com <admin3oo3>
2014-06-16 22:31:34 +0700

    Change version to 1.0.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("Helpers")]
-[assembly: AssemblyDescription("Helps to do some rutin work")]
+[assembly: AssemblyDescription("Helps to do some routin work")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("no company. For own use only.")]
 [assembly: AssemblyProduct("Helpers")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
