ff6545db984f2e8de2530c7e4e71c8f5594333b0
Stepan Sychev <stepan.sychev@gmail.com>
2014-06-20 14:39:06 +0700

    Set version and description for assembly

--- Helpers.Controls/Properties/AssemblyInfo.cs
+++ Helpers.Controls/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Helpers.Controls")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helps to do some routin work")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("no company. For own use only.")]
 [assembly: AssemblyProduct("Helpers.Controls")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Sychev Stepan ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
