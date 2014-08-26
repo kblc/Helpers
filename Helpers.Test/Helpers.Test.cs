568d697ac3525011b9b3de9e7a6157f2f15605b8
Stepan Sychev <stepan.sychev@gmail.com>
2014-08-26 15:03:27 +0700

    Added ConcurrencyObjects and tests for this class.

--- Helpers.Test/Helpers.Test.cs
+++ Helpers.Test/Helpers.Test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
 
 namespace Helpers.Test
 {
@@ -15,6 +16,49 @@ namespace Helpers.Test
             Assert.AreEqual(fileName, Helpers.Log.LogFileName);
             Helpers.Log.Add("First test record.");
             Helpers.Log.Clear();
+        }
+
+        private class ConcurrencyTestClass
+        {
+            public string testString = string.Empty;
+            public ConcurrencyTestClass() { }
+        }
+
+        [TestMethod]
+        public void ConcurrencyObjects()
+        {
+            bool th1End = false;
+            bool th2End = true;
+            bool th1First = false;
+
+            ConcurrencyObjects<ConcurrencyTestClass> co = new ConcurrencyObjects<ConcurrencyTestClass>();
+            co.Max = 1;
+            co.TimeToGetObject = new TimeSpan(0,0,10,0);
+            var str1 = co.GetObject();
+            str1.testString = "test string 1";
+            var th = new Thread(() =>
+            {
+                Console.WriteLine("Wait thread start");
+                Thread.Sleep(5*1000);
+                Console.WriteLine("Wait thread end");
+                co.ReturnObject(str1);
+                Console.WriteLine("Object (1) returns to list");
+                th1End = true;
+                if (th2End == false)
+                    th1First = true;
+            });
+            th.IsBackground = true;
+            th.Start();
+
+            th2End = false;
+            Console.WriteLine("Try to get object (2)");
+            var str2 = co.GetObject();
+            Console.WriteLine(@"Object (2) getted : '{0}'", str1.testString);
+            co.ReturnObject(str2);
+            Console.WriteLine("Object (2) returns to list");
+            th2End = true;
+
+            Assert.AreEqual(th1First && th1End && th2End, true);
         }
     }
 }
---
+++ Helpers/Helpers.ConcurrencyObjects.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Helpers
+{
+    public class ConcurrencyObjects<T>
+    {
+        private int max = 0;
+        public int Max
+        {
+            get { return max; }
+            set { if (value < 0) throw new ArgumentOutOfRangeException(@"Max value can't be less 0"); max = value; }
+        }
+
+        private ConcurrentDictionary<T, bool> objectsDictionary = new ConcurrentDictionary<T, bool>();
+
+        private TimeSpan timeToGetObject = new TimeSpan(0,0,0,0,1000);
+        public TimeSpan TimeToGetObject
+        {
+            get { return timeToGetObject; }
+            set 
+            { 
+                if (timeToGetObject == value) 
+                    return;
+                timeToGetObject = value;
+            }
+        }
+
+        public ConcurrencyObjects()
+        {
+
+        }
+
+
+        private object lockGetObject = new object();
+        public T GetObject()
+        {
+            T result = default(T);
+
+            lock(lockGetObject)
+            {
+                result = objectsDictionary
+                            .Where(i => i.Value == false)
+                            .Select(i => i.Key)
+                            .FirstOrDefault();
+
+                if (result == null)
+                {
+                    var dt = DateTime.Now;
+                    while (objectsDictionary.Count >= Max && Max > 0 && (dt + timeToGetObject > DateTime.Now) && result == null)
+                    { 
+                        Thread.Sleep(20);
+                        result = objectsDictionary
+                                    .Where(i => i.Value == false)
+                                    .Select(i => i.Key)
+                                    .FirstOrDefault();
+                    }
+
+                    if (result == null)
+                    { 
+                        if (Max == 0 || objectsDictionary.Count < Max)
+                        {
+                            result = GetNewObject();
+                            objectsDictionary.TryAdd(result, true);
+                        }
+                        else
+                            throw new Exception(string.Format(@"Can't get object. Time is over."));
+                    } else
+                        objectsDictionary.TryUpdate(result, true, false);
+                }
+            }
+            return result;
+        }
+
+        public void ReturnObject(T obj)
+        {
+            objectsDictionary.TryUpdate(obj, false, true);
+        }
+
+        protected virtual T GetNewObject()
+        {
+            T result = default(T);
+            var ci = typeof(T).GetConstructor(new Type[] { });
+            if (ci != null)
+                result = (T)ci.Invoke(new object[] { });
+            return result;
+        }
+    }
+}
