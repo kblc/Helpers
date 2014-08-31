55873c7caeaf14754ff3a0964d7b59db57e8d3c4
stepan.sychev@gmail.com <admin3oo3>
2014-08-31 23:47:08 +0700

    Added: Is(Not)EqualsVisibilityConverter

---
+++ Helpers.WPF/Converters/IsEqualVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+
+namespace Helpers.WPF.Converters
+{
+    public class IsEqualVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool res = (bool)new IsEqualsConverter().Convert(value, targetType, parameter, culture);
+            return new System.Windows.Controls.BooleanToVisibilityConverter().Convert(res, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
