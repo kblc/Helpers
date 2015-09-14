5fb4a93626de75babcbb06ff1c6748aad6c181ac
stepan.sychev@gmail.com <admin3oo3>
2015-09-15 00:35:42 +0600

    Small fixes for helpers

--- Helpers/Helpers.CSV.cs
+++ Helpers/Helpers.CSV.cs
@@ -13,106 +13,19 @@ namespace Helpers.CSV
 {
     public class CSVFile
     {
+        #region Result fields
+
         public DataTable Table { get; private set; }
         public int TotalRowCount { get; private set; }
         public int ProcessedRowCount { get; private set; }
         public string FilePath { get; private set; }
 
+        #endregion
+
         private CSVFile() { }
 
-        private static string ClearField(string field)
-        {
-            string result = field;
-            if (!string.IsNullOrEmpty(field))
-            {
-                if (result[0] == '\"')
-                    result = result.Remove(0, 1);
-                if (result.Length > 0 && result[result.Length - 1] == '\"')
-                    result = result.Remove(result.Length - 1, 1);
-            }
-            return result;
-        }
-
-        private static string[] GetCsvFields(string line, string delimiter)
-        {
-            List<string> result = new List<string>();
-
-            while (true)
-            {
-                if (line.Length > 0 && line[0] == '"')
-                {
-                    int ind = -1;
-                    if (line.Length > 1)
-                        for (int i = 1; i < line.Length; i++)
-                            if (line[i] == '"'
-                                  && (i == line.Length - 1 || line[i + 1] == delimiter[0])
-                                  && (i == 1 || line[i - 1] != '"' || (i > 2 && line[i - 1] == '"' && line[i - 2] == '"'))
-                                  && (i == line.Length - 1 || line[i + 1] != '"')
-                                )
-                            {
-                                ind = i;
-                                break;
-                            }
-
-                    if (ind > 0)
-                    {
-                        var res = line.Substring(1, ind - 1).Replace("\"\"", "\"");
-                        line = line.Remove(0, ind + 1);
-                        line = (line == string.Empty) ? null : line.Remove(0, 1);
-                        result.Add(res);
-                    }
-                    else
-                    {
-                        var res = ReadUnquotedField(ref line, delimiter);
-                        if (res != null)
-                            result.Add(res);
-                        //throw new ApplicationException("Неверный формат входной строки");
-                    }
-                }
-                else
-                {
-                    var res = ReadUnquotedField(ref line, delimiter);
-                    if (res != null)
-                        result.Add(res);
-                }
-
-                if (line == null)
-                    break;
-            }
-
-            return result.ToArray();
-        }
-
-        private static string ReadUnquotedField(ref string line, string delimiter)
-        {
-            string result = null;
-            if (line != null)
-            {
-                var ind = line.IndexOf(delimiter);
-                if (ind > 0)
-                {
-                    result = line.Substring(0, ind);
-                    line = line.Remove(0, ind + 1);
-                }
-                else if (ind == 0)
-                {
-                    line = line.Remove(0, ind + 1);
-                    result = string.Empty;
-                }
-                else
-                {
-                    result = line;
-                    line = null;
-                }
-
-                //if (ind < 0 && line == string.Empty && result == string.Empty)
-                //    result = null;
-            }
-            return result;
-        }
-
         public static CSVFile Load(
-            string[] lines,
+            IEnumerable<string> lines,
             string tableName,
             string filePath,
             bool hasColumns = true,
@@ -128,19 +41,21 @@ namespace Helpers.CSV
             rowValidator = rowValidator ?? new Action<DataRow>((row) => { });
             log("start load");
 
-            log(string.Format("total lines readed from file: '{0}'", lines.Length));
+            log(string.Format("total lines readed from file: '{0}'", lines.Count()));
 
             var res = new CSVFile()
             {
                 Table = new DataTable(tableName),
                 FilePath = filePath,
                 ProcessedRowCount = 0,
-                TotalRowCount = lines.Length
+                TotalRowCount = lines.Count()
             };
+
+            lines = lines.Where(l => !string.IsNullOrWhiteSpace(l));
 
             try
             {
-                foreach (string line in lines.Where(l => !string.IsNullOrWhiteSpace(l)))
+                foreach (string line in lines)
                 {
                     bool needProceedRow = true;
                     var fields = GetCsvFields(line, delimiter).Select(f => ClearField(f)).ToArray();
@@ -226,7 +141,13 @@ namespace Helpers.CSV
             return Load(lines: lines, tableName: tableName ?? Path.GetFileName(filePath), filePath: filePath, verboseLogAction: log, hasColumns: hasColumns, delimiter: delimiter, columnRenamer: columnRenamer, tableValidator: tableValidator, rowValidator: rowValidator);
         }
 
-        public static IEnumerable<string> Save(DataTable table, bool hasColumns = true, string delimiter = ";", Action<string> verboseLogAction = null, Func < string, string> columnRenamer = null, Func < DataColumn, bool> excludeColumn = null)
+        public static IEnumerable<string> Save(
+            DataTable table, 
+            bool hasColumns = true, 
+            string delimiter = ";", 
+            Action<string> verboseLogAction = null, 
+            Func<string, string> columnRenamer = null, 
+            Func<DataColumn, bool> excludeColumn = null)
         {
             verboseLogAction = verboseLogAction ?? new Action<string>(s => { });
             excludeColumn = excludeColumn ?? new Func<DataColumn, bool>(c => false);
@@ -283,7 +204,15 @@ namespace Helpers.CSV
             return lines;
         }
 
-        public static CSVFile Save(DataTable table, string filePath, Encoding encoding = null, bool hasColumns = true, string delimiter = ";", Action<string> verboseLogAction = null, Func < string, string> columnRenamer = null, Func<DataColumn, bool> excludeColumn = null)
+        public static CSVFile Save(
+            DataTable table, 
+            string filePath, 
+            Encoding encoding = null, 
+            bool hasColumns = true, 
+            string delimiter = ";", 
+            Action<string> verboseLogAction = null, 
+            Func<string, string> columnRenamer = null, 
+            Func<DataColumn, bool> excludeColumn = null)
         {
             verboseLogAction = verboseLogAction ?? new Action<string>(s => { });
             encoding = encoding ?? Encoding.Default;
@@ -317,6 +246,96 @@ namespace Helpers.CSV
             return res;
         }
 
+        #region Additional private
+        private static string ClearField(string field)
+        {
+            string result = field;
+            if (!string.IsNullOrEmpty(field))
+            {
+                if (result[0] == '\"')
+                    result = result.Remove(0, 1);
+                if (result.Length > 0 && result[result.Length - 1] == '\"')
+                    result = result.Remove(result.Length - 1, 1);
+            }
+            return result;
+        }
+        private static string[] GetCsvFields(string line, string delimiter)
+        {
+            List<string> result = new List<string>();
+
+            while (true)
+            {
+                if (line.Length > 0 && line[0] == '"')
+                {
+                    int ind = -1;
+                    if (line.Length > 1)
+                        for (int i = 1; i < line.Length; i++)
+                            if (line[i] == '"'
+                                  && (i == line.Length - 1 || line[i + 1] == delimiter[0])
+                                  && (i == 1 || line[i - 1] != '"' || (i > 2 && line[i - 1] == '"' && line[i - 2] == '"'))
+                                  && (i == line.Length - 1 || line[i + 1] != '"')
+                                )
+                            {
+                                ind = i;
+                                break;
+                            }
+
+                    if (ind > 0)
+                    {
+                        var res = line.Substring(1, ind - 1).Replace("\"\"", "\"");
+                        line = line.Remove(0, ind + 1);
+                        line = (line == string.Empty) ? null : line.Remove(0, 1);
+                        result.Add(res);
+                    }
+                    else
+                    {
+                        var res = ReadUnquotedField(ref line, delimiter);
+                        if (res != null)
+                            result.Add(res);
+                        //throw new ApplicationException("Неверный формат входной строки");
+                    }
+                }
+                else
+                {
+                    var res = ReadUnquotedField(ref line, delimiter);
+                    if (res != null)
+                        result.Add(res);
+                }
+
+                if (line == null)
+                    break;
+            }
+
+            return result.ToArray();
+        }
+        private static string ReadUnquotedField(ref string line, string delimiter)
+        {
+            string result = null;
+            if (line != null)
+            {
+                var ind = line.IndexOf(delimiter);
+                if (ind > 0)
+                {
+                    result = line.Substring(0, ind);
+                    line = line.Remove(0, ind + 1);
+                }
+                else if (ind == 0)
+                {
+                    line = line.Remove(0, ind + 1);
+                    result = string.Empty;
+                }
+                else
+                {
+                    result = line;
+                    line = null;
+                }
+
+                //if (ind < 0 && line == string.Empty && result == string.Empty)
+                //    result = null;
+            }
+            return result;
+        }
+
         private static void WritePreamble(Stream stream, Encoding encoding)
         {
             byte[] preamble = encoding.GetPreamble();
@@ -332,11 +351,21 @@ namespace Helpers.CSV
             byte[] string_arr = encoding.GetBytes(line + Environment.NewLine);
             stream.Write(string_arr, 0, string_arr.Length);
         }
+        #endregion
 
-        public static DataTable MergeTables(IEnumerable<DataTable> dataTables, string[] columnsId)
+        /// <summary>
+        /// Merge tables to one result table
+        /// </summary>
+        /// <param name="dataTables">Tables to merge</param>
+        /// <param name="columnsId">Identifiers column names</param>
+        /// <returns>Merged data table</returns>
+        public static DataTable MergeTables(IEnumerable<DataTable> dataTables, IEnumerable<string> columnsId)
         {
+            if (dataTables == null)
+                throw new ArgumentNullException("dataTables");
+
             columnsId = columnsId ?? new string[] { };
-            var resultCSV = new DataTable();
+            var result = new DataTable();
 
             var columnNames = dataTables
                 .SelectMany(dt => dt.Columns.OfType<DataColumn>().Select(c => new { c.ColumnName, c.DataType }))
@@ -344,50 +373,15 @@ namespace Helpers.CSV
                 .GroupBy(c => c.ColumnName)
                 .Select(g => new { g.FirstOrDefault().ColumnName, ColumnType = g.Count() > 1 ? typeof(string) : g.FirstOrDefault().DataType })
                 .ToArray();
-            resultCSV.Columns.AddRange(columnNames.Select(cn => new DataColumn(cn.ColumnName, cn.ColumnType)).ToArray());
+            result.Columns.AddRange(columnNames.Select(cn => new DataColumn(cn.ColumnName, cn.ColumnType)).ToArray());
 
-            if (columnsId.Length > 0)
-                resultCSV.PrimaryKey = resultCSV.Columns.OfType<DataColumn>().Where(c => columnsId.Contains(c.ColumnName)).ToArray();
+            if (columnsId.Count() > 0)
+                result.PrimaryKey = result.Columns.OfType<DataColumn>().Where(c => columnsId.Contains(c.ColumnName)).ToArray();
 
             foreach (var dt in dataTables)
-                resultCSV.Merge(dt);
+                result.Merge(dt);
 
-            resultCSV.AcceptChanges();
-
-            //foreach (DataTable tbl in dataTables)
-            //    foreach (DataRow dr in tbl.Rows)
-            //    {
-            //        string select = string.Empty;
-            //        foreach (string colId in columnsId)
-            //            select += (select.Length == 0 ? "" : " AND ") + string.Format("{0} = '{1}'", colId, dr[colId].ToString());
-
-            //        DataRow[] drs_for_delete = resultCSV.Select(select);
-
-            //        DataRow newRow = null;
-            //        if (drs_for_delete.Length == 1)
-            //        {
-            //            newRow = drs_for_delete[0];
-            //        }
-            //        else
-            //        {
-            //            foreach (DataRow dr_for_delete in drs_for_delete)
-            //                resultCSV.Rows.Remove(dr_for_delete);
-            //            newRow = resultCSV.NewRow();
-            //        }
-
-            //        try
-            //        {
-            //            foreach (DataColumn col in tbl.Columns)
-            //                newRow[col.ColumnName] = dr[col.ColumnName];
-            //        }
-            //        finally
-            //        {
-            //            if (newRow.RowState == DataRowState.Detached)
-            //                resultCSV.Rows.Add(newRow);
-            //        }
-            //    }
-            
-            return resultCSV;
+            return result;
         }
     }
 }
