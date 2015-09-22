885a5cf105982d1d9cca9069ccbcc9a37ba3abee
stepan.sychev@gmail.com <admin3oo3>
2015-09-22 11:28:42 +0600

    Add summary for ConcatExtension

--- Helpers/Linq/ConcatExtension.cs
+++ Helpers/Linq/ConcatExtension.cs
@@ -5,8 +5,19 @@ using System.Text;
 
 namespace Helpers.Linq
 {
+    /// <summary>
+    /// Concatination extension for LINQ helper
+    /// </summary>
     public static class ConcatExtension
     {
+        /// <summary>
+        /// Get concatinated string from selector
+        /// </summary>
+        /// <typeparam name="TSelect">Selector type</typeparam>
+        /// <param name="selecter">Selector enumeration</param>
+        /// <param name="resultSelector">Selector function</param>
+        /// <param name="delimiter">Delimiter between strings (e.g. ';' or new string)</param>
+        /// <returns>Concatinated string</returns>
         public static string Concat<TSelect>(this IEnumerable<TSelect> selecter, Func<TSelect, string> resultSelector, string delimiter = "")
         {
             var res = string.Empty;
