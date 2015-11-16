7fa66837dca3cd31f20859e964f34e7cfc558045
stepan.sychev@gmail.com <admin3oo3>
2015-11-16 19:02:13 +0600

    Add comments

--- Helpers/Linq/NodeTraverse.cs
+++ Helpers/Linq/NodeTraverse.cs
@@ -7,7 +7,13 @@ namespace Helpers.Linq
 {
     public static class NodeTraverse
     {
-
+        /// <summary>
+        /// Get all items under tree items
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items">Items collection</param>
+        /// <param name="childSelector">Get childs selector</param>
+        /// <returns>All items under tree items</returns>
         public static IEnumerable<T> Traverse<T>(this IEnumerable<T> items, Func<T, IEnumerable<T>> childSelector)
         {
             var stack = new Stack<T>(items);
@@ -20,6 +26,13 @@ namespace Helpers.Linq
             }
         }
 
+        /// <summary>
+        /// Get all items under tree item
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items">Items collection</param>
+        /// <param name="childSelector">Get childs selector</param>
+        /// <returns>All items under tree item</returns>
         public static IEnumerable<T> Traverse<T>(this T item, Func<T, IEnumerable<T>> childSelector)
         {
             return (new T[] { item }).Traverse<T>(childSelector);
